e12968750afb7bead544583321d352712aafa8f5
David Gilbert <gilbe200@gmail.com>
2016-12-28 11:44:54 -0500

    update

--- my-script.js
+++ my-script.js
@@ -2,7 +2,7 @@ $("#my-button").click(function(){
   
   var myInput = $("#my-input").val();
   
-  var myUrl = "http://pokeapi.co/api/v2/pokemon/" + myInput + "/";
+  var myUrl = "https://pokeapi.co/api/v2/pokemon/" + myInput + "/";
   
   $.ajax({
     method: "GET",
